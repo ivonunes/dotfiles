b9da4d8618581866730b63a6e5a17384351e56b6
Ivo Nunes <ivo@nunes.uk>
2024-06-14 20:04:01 +0100

    Update dotfiles

--- linux.sh
+++ linux.sh
@@ -4,9 +4,3 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_basic_vimrc.sh
 
-# tmux
-rm -rf ~/.tmux
-git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-ln -s -f ~/.tmux/.tmux.conf ~
-cp .tmux.conf.local ~
-
--- macos.sh
+++ macos.sh
@@ -5,7 +5,6 @@
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /bin/zsh
 cp .zshrc ~/.zshrc
 
 # amix vimrc
@@ -13,9 +12,15 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 cp my_configs.vim ~/.vim_runtime/
 
-# tmux
-rm -rf ~/.tmux
-git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-ln -s -f ~/.tmux/.tmux.conf ~
-cp .tmux.conf.local ~
+# basics
+brew install appcleaner logi-options-plus 1password --cask
+
+# dev tools
+brew install iterm2 phpstorm docker font-jetbrains-mono --cask
+
+# games
+brew install xiv-on-mac steam --cask
+
+# music production
+brew install izotope-product-portal --cask
 
